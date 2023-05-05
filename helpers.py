542a1b6ae3a271051b72f4babf995a296f8d9696
Lev Shuster <shusterl@carleton.edu>
2023-05-05 10:18:45 -0500

    made helper function use lambda functions

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
-import pandas as pd
-def get_row_letter(number):
-    return chr(ord('A') + number)
+get_row_letter = lambda number: chr(ord('A') + number)
 
-def get_list_of_row_letters(number):
-    return [(get_row_letter(int((i-26)/26)) if i>=26 else '')+get_row_letter(i%26) for i in range(number)]
+get_list_of_row_letters = lambda number: [
+	(get_row_letter(int((i-26)/26)) if i>=26 else '')
+	+ get_row_letter(i%26) for i in range(number)]
